96c1e508ae8810bf336b8e04f4fdcc5234c12f8a
David Kamholz <lautgesetz@gmail.com>
2013-03-27 18:16:31 -0700

    use new panlex module in teradict

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     config = require('./config'),
+    panlex = require('panlex'),
     http = require('http'),
     path = require('path'),
     fs = require('fs'),
@@ -118,12 +119,12 @@ function op1(req, res, next) {
   if (blank(req.body, ['et0'])) return res.render('index');
   var et0 = normalize(req.body.et0);
   
-  apiRequest('/ex', { lv: [res.lg.lv], tt: [et0] }, function (err, data) {
+  panlex.query('/ex', { lv: [res.lg.lv], tt: [et0] }, function (err, data) {
     if (err) return next(err);
     if (data.resultNum === 0) return res.render('index', { subheading: res.lg.ex_not_found});
     
     var ex0 = data.result[0].ex;
-    apiRequest('/lv', { tr: [ex0], sort: ["lc","vc"] }, function (err, data) {
+    panlex.queryAll('/lv', { tr: [ex0], sort: ["lc","vc"] }, function (err, data) {
       if (err) return next(err);
       if (data.resultNum === 0) res.render('index', { subheading: res.lg.tr_not_found });
       else res.render('op1', { result: data.result, ex0: ex0 });
@@ -138,22 +139,10 @@ function op2(req, res, next) {
   var ex0 = Number(req.body.ex0),
       lv1 = res.locals.lv1 = JSON.parse(req.body.lv1);
   
-  apiRequest('/tr', { ex: [ex0], lv: [lv1.lv], sort: ["tt"] }, function (err, data) {
+  panlex.queryAll('/tr', { ex: [ex0], lv: [lv1.lv], sort: ["tt"] }, function (err, data) {
     if (err) return next(err);
     res.render('op2', { result: data.result });
   });
-}
-
-function apiRequest(url, body, cb) {
-  request({ url: config.api + url, json: body, method: 'POST' },
-    function (err, res, body) {
-      if (err) cb('HTTP error: ' + err);
-      else if (res.statusCode != 200) cb('HTTP error: got status code ' + res.statusCode);
-      else {
-        if (body.status === 'error') cb('PanLex API error: ' + body.error);
-        cb(null, body);
-      }
-    });  
 }
 
 function blank(obj, params) {
